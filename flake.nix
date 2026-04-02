4303ae25209b0fcbaf89b53f739ec99907e3be65
Paul Gusmano <paul.gusmano@outlook.com>
2026-04-02 17:39:52 -0400

    typo

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64linux";
+      system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
       packages.${system} = {
